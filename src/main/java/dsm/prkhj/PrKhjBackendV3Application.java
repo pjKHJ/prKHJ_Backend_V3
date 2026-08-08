fa298b742850e179eb3bc83122fe02c864075d16
tlgms <siheun9@gmail.com>
2026-08-08 21:05:44 +0900

    feat: 기본 SpringBoot 프로젝트 설정

---
+++ src/main/java/dsm/prkhj/PrKhjBackendV3Application.java
@@ -0,0 +1,13 @@
+package dsm.prkhj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrKhjBackendV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrKhjBackendV3Application.class, args);
+    }
+
+}
